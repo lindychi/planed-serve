83082613b349d778184e6f5131aef6e11879614f
lindychi <um0205@naver.com>
2022-10-21 07:56:39 +0900

    Initial commit

---
+++ src/main/java/com/lindychi/planedserve/PlanedServeApplication.java
@@ -0,0 +1,13 @@
+package com.lindychi.planedserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanedServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanedServeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lindychi/planedserve/PlanedServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lindychi.planedserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanedServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
